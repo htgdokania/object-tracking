91857b67669ac395cf93dc66b1802fa4bea9d8f4
htg <dokaniaharsh@gmail.com>
2018-06-19 10:05:31 +0530

    minor changes and addition info added

--- 3.moving cursor/3.move-cursor-with-face-v3.py
+++ 3.moving cursor/3.move-cursor-with-face-v3.py
@@ -1,5 +1,5 @@
 speed=6 #set speed at which cursor moves
-#v2.0
+#v3.0
 #htg program move cursor according to movement of face on screen 
 #CODE WILL WORK ONLY IN WEBCAM PLACED DIRECTLY IN MIDDLE OF DIPLAY
 import cv2
@@ -14,10 +14,13 @@ while True:
     ret,frame=cap.read()
     cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
     cv2.resizeWindow('frame', 640,480)
+    #movement instructions 
     cv2.putText(frame,'UP',(260,100),font,2,(0,255,0),5)
     cv2.putText(frame,'DOWN',(210,470),font,2,(100,100,255),5)
     cv2.putText(frame,'RIGHT',(5,270),font,2,(255,100,100),5)
     cv2.putText(frame,'LEFT',(450,270),font,2,(0,165,255),5)
+    cv2.putText(frame,'move towards screen to left click',(0,30),font,.5,(255,255,255),2)
+    cv2.putText(frame,'move away to right click',(420,30),font,.5,(255,255,255),2)
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)#convert current frame to gray 
     #detect face coordinates x,y,w,h
     faces=face_cascade.detectMultiScale(gray,1.3,5)
@@ -27,9 +30,11 @@ while True:
         roi_color=frame[y:y+h,x:x+w]
        # print(x,y,w,h)
         if w<150:
+            cv2.putText(frame,'move away to right click',(420,30),font,.5,(200,0,0),3)
             print('right_click',w)
             pyautogui.rightClick()
         if w>250:
+            cv2.putText(frame,'move towards screen to left click',(0,30),font,.5,(200,0,0),3)
             print('left_click',w)
             pyautogui.click()
         if x in range(230-255):
@@ -53,8 +58,8 @@ while True:
             pyautogui.moveRel(-speed,0)
     
     #gui
-    cv2.putText(frame,'cursor speed={}'.format(speed),(500,10),font,.5,(0,0,0),2)
-    cv2.putText(frame,'keep head inside the white square for still cursor',(0,10),font,.5,(0,0,0),2)
+    cv2.putText(frame,'cursor speed={}'.format(speed),(500,10),font,.5,(229,255,204),1)
+    cv2.putText(frame,'keep head inside the white square for still cursor',(0,10),font,.5,(229,255,204),1)
     cv2.rectangle(frame,(180,140),(440,400),(255,255,255),2)
     cv2.imshow('frame',frame) #display image
     if cv2.waitKey(25) & 0xFF==ord('q'):
